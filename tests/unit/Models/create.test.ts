e311db44e98fce6bc270882b09bd5bc2ade3fc6a
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-22 17:55:22 -0300

    test: "create" method from CarODM

---
+++ tests/unit/Models/create.test.ts
@@ -0,0 +1,15 @@
+import { expect } from 'chai';
+import { Model } from 'mongoose';
+import Sinon from 'sinon';
+import CarODM from '../../../src/Models/CarODM';
+import carsMocks from '../../mocks/carsMocks';
+
+describe('Unit tests for "create" method from CarODM', function () {
+  const model = new CarODM();
+
+  it('should be able to create new car', async function () {
+    Sinon.stub(Model, 'create').resolves(carsMocks);
+    const result = await model.create(carsMocks);
+    expect(result).to.deep.equal(carsMocks);
+  });
+});

b89c01a20da9c79bf152dbf307af25221fd4d738
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-22 17:43:59 -0300

    refactor: prettier file

--- tests/unit/Services/create.test.ts
+++ tests/unit/Services/create.test.ts
@@ -1,20 +1,24 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
 import { Model } from 'mongoose';
-import carsMocks from '../../mocks/carsMocks'
+import carsMocks from '../../mocks/carsMocks';
 import ICar from '../../../src/Interfaces/ICar';
 import CarService from '../../../src/Services/CarService';
 
 describe('Unit tests for creating a new car', function () {
-  const carService = new CarService()
+  const carService = new CarService();
+
+  afterEach(function () {
+    sinon.restore();
+  });
 
   it('should be able to create a new car', async function () {
     const output: ICar = { ...carsMocks, status: true };
-    const input: ICar = carsMocks
+    const input: ICar = carsMocks;
 
-    sinon.stub(Model, 'create').resolves(output)
+    sinon.stub(Model, 'create').resolves(output);
     const result: ICar = await carService.create(input);
 
-    expect(result).to.deep.equal(output)
+    expect(result).to.deep.equal(output);
   });
 });

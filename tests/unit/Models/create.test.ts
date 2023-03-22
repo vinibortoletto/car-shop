7d66461c05bec7a52875e4bfc754ab768e5c2a1f
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-22 18:02:53 -0300

    test: add Sinon.restore

--- tests/unit/Models/create.test.ts
+++ tests/unit/Models/create.test.ts
@@ -7,6 +7,10 @@ import carsMocks from '../../mocks/carsMocks';
 describe('Unit tests for "create" method from CarODM', function () {
   const model = new CarODM();
 
+  afterEach(function () {
+    Sinon.restore();
+  });
+
   it('should be able to create new car', async function () {
     Sinon.stub(Model, 'create').resolves(carsMocks);
     const result = await model.create(carsMocks);

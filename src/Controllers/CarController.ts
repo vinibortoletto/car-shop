450cab72983761a142e0f8d3ff5635134581bf0e
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-22 19:48:50 -0300

    refactor: "create" method in CarController

--- src/Controllers/CarController.ts
+++ src/Controllers/CarController.ts
@@ -2,21 +2,20 @@ import { NextFunction, Response, Request } from 'express';
 import CarService from '../Services/CarService';
 
 export default class CarController {
-  constructor(
-    private _req: Request,
-    private _res: Response,
-    private _next: NextFunction,
-    private _service: CarService,
-  ) {}
+  constructor(private _service: CarService) {}
 
-  public async create() {
-    const newCar = this._req.body;
+  public async create(
+    req: Request,
+    res: Response, 
+    next: NextFunction,
+  ) {
+    const newCar = req.body;
     
     try {
       const car = await this._service.create(newCar);
-      return this._res.status(201).json(car);
+      return res.status(201).json(car);
     } catch (error) {
-      this._next(error);
+      next(error);
     }
   }
 }

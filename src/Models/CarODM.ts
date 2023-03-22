9a44ae4476fd78de98463af30381529e8bf39adb
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-22 15:59:44 -0300

    feat: create car model

---
+++ src/Models/CarODM.ts
@@ -0,0 +1,25 @@
+import { model, Model, models, Schema } from 'mongoose';
+import ICar from '../Interfaces/ICar';
+
+export default class CarODM {
+  private _schema: Schema;
+  private _model: Model<ICar>;
+  
+  constructor() {
+    this._schema = new Schema<ICar>({
+      model: { type: String, required: true },
+      year: { type: Number, required: true },
+      color: { type: String, required: true },
+      status: { type: Boolean, required: false },
+      buyValue: { type: Number, required: true },
+      doorsQty: { type: Number, required: true },
+      seatsQty: { type: Number, required: true },
+    });
+
+    this._model = models.Car || model('Car', this._schema);
+  }
+
+  public async create(newCar: ICar): Promise<ICar> {
+    return this._model.create(newCar);
+  }
+}

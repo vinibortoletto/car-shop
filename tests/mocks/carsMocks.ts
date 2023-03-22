c1b9e62ee470e3d1d8d0981dbce094566d1b00c9
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-22 19:55:46 -0300

    chore: cars mocks

--- tests/mocks/carsMocks.ts
+++ tests/mocks/carsMocks.ts
@@ -1,10 +1,19 @@
-const car = {
-  model: 'Marea',
-  year: 2002,
-  color: 'Black',
-  buyValue: 15.99,
-  doorsQty: 4,
-  seatsQty: 5,
+export const createCarInput = {
+  model: 'Uno da Escada',
+  year: 1960,
+  color: 'Red',
+  buyValue: 1500,
+  doorsQty: 2,
+  seatsQty: 2,
 };
 
-export default car;
+export const createCarOutput = {
+  id: '641b82d428a938f06b73cefe',
+  model: 'Uno da Escada',
+  year: 1960,
+  color: 'Red',
+  status: false,
+  buyValue: 1500,
+  doorsQty: 2,
+  seatsQty: 2,
+};

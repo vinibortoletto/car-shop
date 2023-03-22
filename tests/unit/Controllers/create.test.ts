853039c29020e0478e38e05cd731bea92ffe2732
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-22 18:02:29 -0300

    refactor: sinon import name

--- tests/unit/Controllers/create.test.ts
+++ tests/unit/Controllers/create.test.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 import { NextFunction, Request, Response } from 'express';
-import sinon, { SinonStub } from 'sinon';
+import Sinon, { SinonStub } from 'sinon';
 import CarController from '../../../src/Controllers/CarController';
 import CarService from '../../../src/Services/CarService';
 import carsMocks from '../../mocks/carsMocks';
@@ -14,16 +14,16 @@ describe('Unit tests for "create" method from CarController', function () {
   const carController = new CarController(req, res, next, carService);
 
   beforeEach(function () {
-    res.status = sinon.stub().returns(res);
-    res.json = sinon.stub().returns(res);
+    res.status = Sinon.stub().returns(res);
+    res.json = Sinon.stub().returns(res);
   });
 
   afterEach(function () {
-    sinon.restore();
+    Sinon.restore();
   });
 
   it('should be able to create new car', async function () {
-    sinon.stub(carService, 'create').resolves(carsMocks);
+    Sinon.stub(carService, 'create').resolves(carsMocks);
     await carController.create();
     expect((res.status as SinonStub).calledWith(201)).to.equal(true);
     expect((res.json as SinonStub).calledWith(carsMocks)).to.equal(true);
--- tests/unit/Services/create.test.ts
+++ tests/unit/Services/create.test.ts
@@ -1,22 +1,22 @@
 import { expect } from 'chai';
-import sinon from 'sinon';
 import { Model } from 'mongoose';
+import Sinon from 'sinon';
 import carsMocks from '../../mocks/carsMocks';
 import ICar from '../../../src/Interfaces/ICar';
 import CarService from '../../../src/Services/CarService';
 
-describe('Unit tests for creating a new car', function () {
+describe('Unit tests for "create" method from CarService', function () {
   const carService = new CarService();
 
   afterEach(function () {
-    sinon.restore();
+    Sinon.restore();
   });
 
   it('should be able to create a new car', async function () {
     const output: ICar = { ...carsMocks, status: true };
     const input: ICar = carsMocks;
 
-    sinon.stub(Model, 'create').resolves(output);
+    Sinon.stub(Model, 'create').resolves(output);
     const result: ICar = await carService.create(input);
 
     expect(result).to.deep.equal(output);

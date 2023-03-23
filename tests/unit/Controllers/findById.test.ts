5aa352e4680e3776ac8def5c5d317c849cfce9ef
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-23 17:10:46 -0300

    test: "findById" method from CarController

--- tests/unit/Controllers/findById.test.ts
+++ tests/unit/Controllers/findById.test.ts
@@ -1,19 +1,22 @@
 import { expect } from 'chai';
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import Sinon, { SinonStub } from 'sinon';
 import CarController from '../../../src/Controllers/CarController';
+import NotFound from '../../../src/Errors/NotFound';
 import CarService from '../../../src/Services/CarService';
+import { carNotFound } from '../../../src/Utils/errorMessages';
 import * as mocks from '../../mocks/carsMocks';
 
 describe('Unit tests for "findById" method from CarController', function () {
   let req = {} as Request;
   const res = {} as Response;
-  const next = (() => {}) as NextFunction;
+  let next: SinonStub;
     
   const service = new CarService();
   const controller = new CarController(service);
 
   beforeEach(function () {
+    next = Sinon.stub() as unknown as SinonStub; 
     res.status = Sinon.stub().returns(res);
     res.json = Sinon.stub().returns(res);
   });
@@ -23,10 +26,21 @@ describe('Unit tests for "findById" method from CarController', function () {
   });
 
   it('should be able to find car by its id', async function () {
-    req = { params: mocks.carId } as unknown as Request;
     Sinon.stub(service, 'findById').resolves(mocks.car);
+    req = { params: mocks.carId } as unknown as Request;
+    
     await controller.findById(req, res, next);
+    
     expect((res.status as SinonStub).calledWith(200)).to.equal(true);
     expect((res.json as SinonStub).calledWith(mocks.car)).to.equal(true);
+  });
+
+  it('should throw NotFound error if car does not exists in the database', async function () {
+    req = { params: 'wrong id' } as unknown as Request;
+    const error = new NotFound(carNotFound);
+    Sinon.stub(service, 'findById').rejects(error);
+
+    await controller.findById(req, res, next);
+    expect(next.calledWith(error)).to.equal(true);
   });
 });

e365fd7fbcde29047ed855c2eaf94df7b0e2f831
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-23 15:02:07 -0300

    chore: cars mocks

--- tests/mocks/carsMocks.ts
+++ tests/mocks/carsMocks.ts
@@ -1,3 +1,5 @@
+import ICar from '../../src/Interfaces/ICar';
+
 export const createCarInput = {
   model: 'Uno da Escada',
   year: 1960,
@@ -17,3 +19,23 @@ export const createCarOutput = {
   doorsQty: 2,
   seatsQty: 2,
 };
+
+export const findCarsOutput: ICar[] = [
+  {
+    model: 'Marea',
+    year: 2002,
+    color: 'Black',
+    status: true,
+    buyValue: 15.990,
+    doorsQty: 4,
+    seatsQty: 5,
+  },
+  {
+    model: 'Tempra',
+    year: 1995,
+    color: 'Black',
+    buyValue: 39.000,
+    doorsQty: 2,
+    seatsQty: 5,
+  },
+];

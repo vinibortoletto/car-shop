a89ecce0bcd6bee429323675e7a3073809e35fe1
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-23 15:16:30 -0300

    chore: car and carId mocks

--- tests/mocks/carsMocks.ts
+++ tests/mocks/carsMocks.ts
@@ -39,3 +39,14 @@ export const findCarsOutput: ICar[] = [
     seatsQty: 5,
   },
 ];
+
+export const carId = '641b82d428a938f06b73cefe';
+
+export const car: ICar = {
+  model: 'Uno da Escada',
+  year: 1960,
+  color: 'Red',
+  buyValue: 1500,
+  doorsQty: 2,
+  seatsQty: 2,
+};

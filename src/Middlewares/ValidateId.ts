4311652db10059fe10a77219c85e045fbe3227fb
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-23 16:14:17 -0300

    feat: ValidateId middleware

---
+++ src/Middlewares/ValidateId.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from 'express';
+import { isValidObjectId } from 'mongoose';
+import UnprocessableContent from '../Errors/UnprocessableContent';
+import { invalidId } from '../Utils/errorMessages';
+
+export default class ValidateMongoId {
+  public static validate(
+    req: Request, 
+    _res: Response, 
+    next: NextFunction,
+  ): void {
+    const { id } = req.params;
+    
+    if (!isValidObjectId(id)) {
+      throw new UnprocessableContent(invalidId);
+    }
+    
+    next();
+  }
+}

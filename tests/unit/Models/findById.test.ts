7a62470481912e76913d3554233ff89a0299a607
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-23 15:15:56 -0300

    test: "findById" method from CarODM

---
+++ tests/unit/Models/findById.test.ts
@@ -0,0 +1,19 @@
+import { expect } from 'chai';
+import { Model } from 'mongoose';
+import Sinon from 'sinon';
+import CarODM from '../../../src/Models/CarODM';
+import * as mocks from '../../mocks/carsMocks';
+
+describe('Unit tests for "findById" method from CarODM', function () {
+  const model = new CarODM();
+
+  afterEach(function () {
+    Sinon.restore();
+  });
+
+  it('should be able to find car by its id', async function () {
+    Sinon.stub(Model, 'findById').resolves(mocks.car);
+    const result = await model.findById(mocks.carId);
+    expect(result).to.deep.equal(mocks.car);
+  });
+});

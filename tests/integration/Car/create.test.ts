def0d2f7599a4450cd1eb58128ed4c8800dbc477
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-23 20:30:26 -0300

    change: test path

--- tests/integration/Car/create.test.ts
+++ tests/integration/Car/create.test.ts
@@ -1,9 +1,9 @@
 import request from 'supertest';
 import { Model } from 'mongoose';
 import Sinon from 'sinon';
-import * as mocks from '../mocks/carsMocks';
-import app from '../../src/app';
-import { CREATED } from '../../src/Utils/httpStatusCodes';
+import * as mocks from '../../mocks/carsMocks';
+import app from '../../../src/app';
+import { CREATED } from '../../../src/Utils/httpStatusCodes';
 
 describe('Integration tests for creating new car', function () {
   it('should be able to create new car', async function () {

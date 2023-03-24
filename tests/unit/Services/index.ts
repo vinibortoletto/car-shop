5dbcdd2bec388c0babef553f87ecfe23f88157a5
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-24 15:08:52 -0300

    chore: services import

---
+++ tests/unit/Services/index.ts
@@ -0,0 +1,7 @@
+import CarService from '../../../src/Services/CarService';
+import MotorcycleService from '../../../src/Services/MotorcycleService';
+
+export {
+  CarService,
+  MotorcycleService,
+};

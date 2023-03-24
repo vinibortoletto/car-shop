a34c166fb480cbcbc16356fbe87b0fa67163cfdc
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-23 22:20:25 -0300

    test: "update" method from CarODM

---
+++ tests/unit/Models/update.test.ts
@@ -0,0 +1,16 @@
+import { expect } from 'chai';
+import { Model } from 'mongoose';
+import Sinon from 'sinon';
+import ICar from '../../../src/Interfaces/ICar';
+import CarODM from '../../../src/Models/CarODM';
+import * as mocks from '../../mocks/carsMocks';
+
+describe('Unit tests for "update" method from CarODM', function () {
+  const model = new CarODM();
+
+  it('should be able to update a car by its id', async function () {
+    Sinon.stub(Model, 'findByIdAndUpdate').resolves(mocks.carList[0]);
+    const result: ICar | null = await model.update(mocks.car, mocks.carId);
+    expect(result).to.deep.equal(mocks.carList[0]);
+  });
+});

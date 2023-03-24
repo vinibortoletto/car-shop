ed2b8a971c9c69abca562d01bad6313906226815
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-23 21:46:05 -0300

    chore: ErrorHandle import

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import ErrorHandler from './Middlewares/ErrorHandler';
 import router from './Routes/carsRoute';
 
 const app = express();
 app.use(express.json());
 app.use(router);
+app.use(ErrorHandler.handle);
 
 export default app;

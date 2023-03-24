0ed9b14b79738866ab5b33b1244b7c205c35da2a
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-24 17:28:38 -0300

    feat: GET /motorcycles/:id

--- src/Routers/motorcycleRouter.ts
+++ src/Routers/motorcycleRouter.ts
@@ -8,5 +8,6 @@ const controller = new MotorcycleController(service);
 
 motorcycleRouter.post('/motorcycles', controller.create.bind(controller));
 motorcycleRouter.get('/motorcycles', controller.find.bind(controller));
+motorcycleRouter.get('/motorcycles/:id', controller.findById.bind(controller));
 
 export default motorcycleRouter;

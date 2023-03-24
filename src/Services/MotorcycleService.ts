36c766f78c1a2445b4ced4c8edb12fce83aa363b
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-24 15:44:34 -0300

    feat: "find" method in MotorcycleService

--- src/Services/MotorcycleService.ts
+++ src/Services/MotorcycleService.ts
@@ -5,7 +5,9 @@ import { MotorcycleODM } from '../Models';
 export default class MotorcycleService {
   private _model = new MotorcycleODM();
 
-  private createMotorcycleDomain(motorcycle: IMotorcycle | null): Motorcycle | null {
+  private createMotorcycleDomain(
+    motorcycle: IMotorcycle | null,
+  ): Motorcycle | null {
     if (!motorcycle) return null;
     return new Motorcycle(motorcycle);
   }
@@ -13,5 +15,13 @@ export default class MotorcycleService {
   public async create(newMotorcycle: IMotorcycle): Promise<Motorcycle | null> {
     const motorcycle: IMotorcycle = await this._model.create(newMotorcycle);
     return this.createMotorcycleDomain(motorcycle);
+  }
+
+  public async find(): Promise<(Motorcycle | null)[]> {
+    const motorcycleList: IMotorcycle[] = await this._model.find();
+    const motorcycleDomainList: (Motorcycle | null)[] = motorcycleList.map(
+      (motorcycle) => this.createMotorcycleDomain(motorcycle),
+    );
+    return motorcycleDomainList;
   }
 }

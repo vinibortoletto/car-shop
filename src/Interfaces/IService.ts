3fa32605437f41a6fa8aa02f90d0cd5a81481b07
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-24 17:13:48 -0300

    feat: IService interface

---
+++ src/Interfaces/IService.ts
@@ -0,0 +1,6 @@
+export default interface IService<T, U> {
+  create(newObj: T): Promise<U | null>;
+  find(): Promise<(U | null)[]>;
+  findById(id: string): Promise<U | null>;
+  findByIdAndUpdate(newObj: T, id: string): Promise<U | null>;
+}
--- src/Interfaces/index.ts
+++ src/Interfaces/index.ts
@@ -1,9 +1,11 @@
 import IVehicle from './IVehicle';
 import ICar from './ICar';
 import IMotorcycle from './IMotorcycle';
+import IService from './IService';
 
 export {
   IVehicle,
   ICar,
   IMotorcycle,
+  IService,
 };

e50a7ee9173ca371df5549697f6bad3af9870dae
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-24 20:51:38 -0300

    refactor: Car/Motorcycle extends from AbstractController

--- src/Controllers/CarController.ts
+++ src/Controllers/CarController.ts
@@ -1,83 +1,13 @@
-import { NextFunction, Response, Request } from 'express';
 import { Car } from '../Domains';
 import { ICar } from '../Interfaces';
-import CarService from '../Services/CarService';
-import { CREATED, NO_CONTENT, OK } from '../Utils/httpStatusCodes';
+import { CarService } from '../Services';
+import AbstractController from './AbstractController';
 
-export default class CarController {
-  constructor(private _service: CarService) {}
-
-  public async create(
-    req: Request,
-    res: Response, 
-    next: NextFunction,
-  ) {
-    const newCar: ICar = req.body;
-    
-    try {
-      const car: Car | null = await this._service.create(newCar);
-      return res.status(CREATED).json(car);
-    } catch (error) {
-      next(error);
-    }
-  }
-
-  public async find(
-    req: Request,
-    res: Response, 
-    next: NextFunction,
-  ) {
-    try {
-      const carList: (Car | null)[] = await this._service.find();
-      return res.status(OK).json(carList);
-    } catch (error) {
-      next(error);
-    }
-  }
-
-  public async findById(
-    req: Request,
-    res: Response, 
-    next: NextFunction,
-  ) {
-    const { id } = req.params;
-
-    try {
-      const car: Car | null = await this._service.findById(id);
-      return res.status(OK).json(car);
-    } catch (error) {
-      next(error);
-    }
-  }
-
-  public async findByIdAndUpdate(
-    req: Request,
-    res: Response, 
-    next: NextFunction,
-  ) {
-    const { id } = req.params;
-    const newCar: ICar = req.body;
-
-    try {
-      const car: Car | null = await this._service.findByIdAndUpdate(newCar, id);
-      return res.status(OK).json(car);
-    } catch (error) {
-      next(error);
-    }
-  }
-
-  public async findByIdAndDelete(
-    req: Request,
-    res: Response, 
-    next: NextFunction,
-  ) {
-    const { id } = req.params;
-
-    try {
-      await this._service.findByIdAndDelete(id);
-      return res.status(NO_CONTENT).json();
-    } catch (error) {
-      next(error);
-    }
+export default class CarController extends AbstractController<ICar, Car> {
+  protected _service: CarService;
+  
+  constructor(service: CarService) {
+    super(service);
+    this._service = service;
   }
 }
--- src/Controllers/MotorcycleController.ts
+++ src/Controllers/MotorcycleController.ts
@@ -1,86 +1,13 @@
-import { NextFunction, Response, Request } from 'express';
 import { Motorcycle } from '../Domains';
 import { IMotorcycle } from '../Interfaces';
-import MotorcycleService from '../Services/MotorcycleService';
-import { CREATED, NO_CONTENT, OK } from '../Utils/httpStatusCodes';
+import { MotorcycleService } from '../Services';
+import AbstractController from './AbstractController';
 
-export default class MotorcycleController {
-  constructor(private _service: MotorcycleService) {}
-
-  public async create(
-    req: Request,
-    res: Response, 
-    next: NextFunction,
-  ) {
-    const newMotorcycle = req.body;
-    
-    try {
-      const motorcycle = await this._service.create(newMotorcycle);
-      return res.status(CREATED).json(motorcycle);
-    } catch (error) {
-      next(error);
-    }
-  }
-
-  public async find(
-    req: Request,
-    res: Response, 
-    next: NextFunction,
-  ) {
-    try {
-      const motorcycleList = await this._service.find();
-      return res.status(OK).json(motorcycleList);
-    } catch (error) {
-      next(error);
-    }
-  }
-
-  public async findById(
-    req: Request,
-    res: Response, 
-    next: NextFunction,
-  ) {
-    const { id } = req.params;
-
-    try {
-      const motorcycle: Motorcycle | null = await this._service.findById(id);
-      return res.status(OK).json(motorcycle);
-    } catch (error) {
-      next(error);
-    }
-  }
-
-  public async findByIdAndUpdate(
-    req: Request,
-    res: Response, 
-    next: NextFunction,
-  ) {
-    const { id } = req.params;
-    const newMotorcycle: IMotorcycle = req.body;
-
-    try {
-      const motorcycle: Motorcycle | null = await this
-        ._service
-        .findByIdAndUpdate(newMotorcycle, id);
-
-      return res.status(OK).json(motorcycle);
-    } catch (error) {
-      next(error);
-    }
-  }
-
-  public async findByIdAndDelete(
-    req: Request,
-    res: Response, 
-    next: NextFunction,
-  ) {
-    const { id } = req.params;
-
-    try {
-      await this._service.findByIdAndDelete(id);
-      return res.status(NO_CONTENT).json();
-    } catch (error) {
-      next(error);
-    }
+export default class MotorcycleController extends AbstractController<IMotorcycle, Motorcycle> {
+  protected _service: MotorcycleService;
+  
+  constructor(service: MotorcycleService) {
+    super(service);
+    this._service = service;
   }
 }

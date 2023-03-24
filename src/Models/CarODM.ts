ebaf7da42be55bb7cccb5bd409c3b7edf81e5619
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-24 00:03:33 -0300

    refactor: CarODM extends from AbstractODM

--- src/Models/CarODM.ts
+++ src/Models/CarODM.ts
@@ -1,12 +1,10 @@
-import { model, Model, models, Schema } from 'mongoose';
+import { Schema } from 'mongoose';
 import ICar from '../Interfaces/ICar';
+import AbstractODM from './AbstractODM';
 
-export default class CarODM {
-  private _schema: Schema;
-  private _model: Model<ICar>;
-  
+export default class CarODM extends AbstractODM<ICar> {
   constructor() {
-    this._schema = new Schema<ICar>({
+    const schema = new Schema<ICar>({
       model: { type: String, required: true },
       year: { type: Number, required: true },
       color: { type: String, required: true },
@@ -16,22 +14,6 @@ export default class CarODM {
       seatsQty: { type: Number, required: true },
     });
 
-    this._model = models.Car || model('Car', this._schema);
-  }
-
-  public async create(newCar: ICar): Promise<ICar> {
-    return this._model.create(newCar);
-  }
-
-  public async find(): Promise<ICar[]> {
-    return this._model.find();
-  }
-
-  public async findById(id: string): Promise<ICar | null> {
-    return this._model.findById(id);
-  }
-
-  public async findByIdAndUpdate(newCar: ICar, id: string): Promise<ICar | null> {
-    return this._model.findByIdAndUpdate(id, newCar, { new: true });
+    super(schema, 'Car');
   }
 }

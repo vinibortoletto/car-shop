00366990252dc6604adf5b1f1fbac498351d9073
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-24 00:03:14 -0300

    feat: AbstractODM

---
+++ src/Models/AbstractODM.ts
@@ -0,0 +1,31 @@
+import { model, Model, models, Schema, UpdateQuery } from 'mongoose';
+
+export default class AbstractODM<T> {
+  protected _model: Model<T>;
+
+  constructor(
+    protected _schema: Schema,
+    protected _modelName: string,
+  ) {
+    this._model = models.Car || model(this._modelName, this._schema);
+  }
+
+  public async create(obj: T): Promise<T> {
+    return this._model.create(obj);
+  }
+
+  public async find(): Promise<T[]> {
+    return this._model.find();
+  }
+
+  public async findById(id: string): Promise<T | null> {
+    return this._model.findById(id);
+  }
+
+  public async findByIdAndUpdate(
+    obj: UpdateQuery<T>, 
+    id: string,
+  ): Promise<T | null> {
+    return this._model.findByIdAndUpdate(id, obj, { new: true });
+  }
+}

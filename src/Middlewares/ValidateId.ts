4355b6fca2869fa68414bc2d81f630759bb80045
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-23 21:46:20 -0300

    fix: ValidateId class name

--- src/Middlewares/ValidateId.ts
+++ src/Middlewares/ValidateId.ts
@@ -3,7 +3,7 @@ import { isValidObjectId } from 'mongoose';
 import UnprocessableContent from '../Errors/UnprocessableContent';
 import { invalidId } from '../Utils/errorMessages';
 
-export default class ValidateMongoId {
+export default class ValidateId {
   public static validate(
     req: Request, 
     _res: Response, 

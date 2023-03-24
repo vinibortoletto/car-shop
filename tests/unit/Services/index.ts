2bc1ed886778bac20dff7631be85939194d6d12e
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-24 15:32:26 -0300

    refactor: remove tests barrel import

--- tests/unit/Services/index.ts
+++
@@ -1,7 +0,0 @@
-import CarService from '../../../src/Services/CarService';
-import MotorcycleService from '../../../src/Services/MotorcycleService';
-
-export {
-  CarService,
-  MotorcycleService,
-};

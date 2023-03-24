99871a8b81e85ca0ee58ae42be661e05e1c01b09
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-24 15:44:14 -0300

    test: "find" method from MotorcycleController

--- tests/unit/Controllers/MotorcycleController.test.ts
+++ tests/unit/Controllers/MotorcycleController.test.ts
@@ -4,7 +4,7 @@ import Sinon, { SinonStub } from 'sinon';
 import { MotorcycleController } from '../../../src/Controllers';
 import { Motorcycle } from '../../../src/Domains';
 import { MotorcycleService } from '../../../src/Services';
-import { CREATED } from '../../../src/Utils/httpStatusCodes';
+import { CREATED, OK } from '../../../src/Utils/httpStatusCodes';
 import { motorcycleList } from '../../mocks/motorcyclesMocks';
 
 describe('Unit tests for "MotorcycleController" class', function () {
@@ -40,6 +40,25 @@ describe('Unit tests for "MotorcycleController" class', function () {
       const error = new Error('Internal Server Error');
       Sinon.stub(service, 'create').rejects(error);
       await controller.create(req, res, next);
+      expect(next.calledWith(error)).to.equal(true);
+    });
+  });
+
+  describe('"find" method', function () {
+    it('should be able to find all motorcycles', async function () {
+      const output: Motorcycle[] = motorcycleList.map(
+        (motorcycle) => new Motorcycle(motorcycle),
+      );
+      Sinon.stub(service, 'find').resolves(output);
+      await controller.find(req, res, next);
+      expect((res.status as SinonStub).calledWith(OK)).to.equal(true);
+      expect((res.json as SinonStub).calledWith(output)).to.equal(true);
+    });
+  
+    it('should throw Internal Server Error', async function () {
+      const error = new Error('Internal Server Error');
+      Sinon.stub(service, 'find').rejects(error);
+      await controller.find(req, res, next);
       expect(next.calledWith(error)).to.equal(true);
     });
   });

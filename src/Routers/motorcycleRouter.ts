0638acc09a8c5e2b7fab9a999bd32c0c2fe67010
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-24 17:31:59 -0300

    fix: add middleware to route

--- src/Routers/motorcycleRouter.ts
+++ src/Routers/motorcycleRouter.ts
@@ -1,13 +1,26 @@
 import { Router } from 'express';
 import MotorcycleController from '../Controllers/MotorcycleController';
+import ValidateId from '../Middlewares/ValidateId';
 import MotorcycleService from '../Services/MotorcycleService';
 
 const motorcycleRouter = Router();
 const service = new MotorcycleService();
 const controller = new MotorcycleController(service);
 
-motorcycleRouter.post('/motorcycles', controller.create.bind(controller));
-motorcycleRouter.get('/motorcycles', controller.find.bind(controller));
-motorcycleRouter.get('/motorcycles/:id', controller.findById.bind(controller));
+motorcycleRouter.post(
+  '/motorcycles', 
+  controller.create.bind(controller),
+);
+
+motorcycleRouter.get(
+  '/motorcycles', 
+  controller.find.bind(controller),
+);
+
+motorcycleRouter.get(
+  '/motorcycles/:id',
+  ValidateId.validate,
+  controller.findById.bind(controller),
+);
 
 export default motorcycleRouter;

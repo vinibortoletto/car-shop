397ce0e5a0dcf09dad130d2648127f6ed4bccef3
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-23 22:44:44 -0300

    test: "findByIdAndUpdate" fail case integration

--- tests/integration/Car/findByIdAndUpdate.test.ts
+++ tests/integration/Car/findByIdAndUpdate.test.ts
@@ -3,7 +3,8 @@ import { Model } from 'mongoose';
 import Sinon from 'sinon';
 import * as mocks from '../../mocks/carsMocks';
 import app from '../../../src/app';
-import { OK } from '../../../src/Utils/httpStatusCodes';
+import { NOT_FOUND, OK, UNPROCESSABLE_CONTENT } from '../../../src/Utils/httpStatusCodes';
+import { carNotFound, invalidId } from '../../../src/Utils/errorMessages';
 
 describe('Integration tests for updating a car by its id', function () {
   afterEach(function () {
@@ -17,5 +18,25 @@ describe('Integration tests for updating a car by its id', function () {
       .put(`/cars/${mocks.carId}`)
       .expect(OK)
       .expect(mocks.carList[0]);
+  });
+
+  it('should throw NotFound error if car does not exists in the database', async function () {
+    Sinon.stub(Model, 'findByIdAndUpdate').resolves(null);
+
+    await request(app)
+      .put(`/cars/${mocks.carId}`)
+      .send(mocks.car)
+      .expect(NOT_FOUND)
+      .expect({ message: carNotFound });
+  });
+
+  it('should throw UnprocessableContent error if id is invalid', async function () {
+    Sinon.stub(Model, 'findByIdAndUpdate').resolves(null);
+
+    await request(app)
+      .put('/cars/invalid-id')
+      .send(mocks.car)
+      .expect(UNPROCESSABLE_CONTENT)
+      .expect({ message: invalidId });
   });
 });

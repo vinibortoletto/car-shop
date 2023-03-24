d306dee2f7e37dbae74d3e661d79e2ad0c86b942
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-24 15:31:24 -0300

    refactor: rename carsRouter

--- src/Routers/carsRouter.ts
+++ src/Routers/carsRouter.ts
@@ -3,22 +3,22 @@ import CarController from '../Controllers/CarController';
 import ValidateId from '../Middlewares/ValidateId';
 import CarService from '../Services/CarService';
 
-const router = Router();
+const carRouter = Router();
 const service = new CarService();
 const controller = new CarController(service);
 
-router.post('/cars', controller.create.bind(controller));
-router.get('/cars', controller.find.bind(controller));
-router.get(
+carRouter.post('/cars', controller.create.bind(controller));
+carRouter.get('/cars', controller.find.bind(controller));
+carRouter.get(
   '/cars/:id', 
   ValidateId.validate,
   controller.findById.bind(controller),
 );
 
-router.put(
+carRouter.put(
   '/cars/:id', 
   ValidateId.validate,
   controller.findByIdAndUpdate.bind(controller),
 );
 
-export default router;
+export default carRouter;

443ac15d0b928dd3078c52a0fbe4863c3c67c6c6
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-24 21:59:33 -0300

    refactor: remove unused interface

--- src/Interfaces/IService.ts
+++
@@ -1,7 +0,0 @@
-export default interface IService<T, U> {
-  create(newObj: T): Promise<U | null>;
-  find(): Promise<(U | null)[]>;
-  findById(id: string): Promise<U | null>;
-  findByIdAndUpdate(newObj: T, id: string): Promise<U | null>;
-  findByIdAndDelete(id:string): Promise<boolean>;
-}
--- src/Interfaces/index.ts
+++ src/Interfaces/index.ts
@@ -1,11 +1,9 @@
 import IVehicle from './IVehicle';
 import ICar from './ICar';
 import IMotorcycle from './IMotorcycle';
-import IService from './IService';
 
 export {
   IVehicle,
   ICar,
   IMotorcycle,
-  IService,
 };
